4d777424d05e548eba821ea08ead8d6e26f6a4d1
Alwaysproblem <reganyang0415@gmail.com>
2020-05-16 17:59:45 +0800

    bin

--- encode.py
+++ encode.py
@@ -1,4 +1,5 @@
 import argparse
+import random
 
 def argument_sys():
     parser = argparse.ArgumentParser()
@@ -8,7 +9,7 @@ def argument_sys():
 
 def input_file(filename):
     try:
-        with open(filename, 'r+') as f:
+        with open(filename, 'rb') as f:
             content = f.read()
     except FileNotFoundError:
         print("I don't find file.")
@@ -18,12 +19,17 @@ def main():
     arg = argument_sys()
     # print(arg.filename)
     content = input_file(arg.filename)
-    # print(content)
-    c = [ord(i)^3 for i in content]
-    d = [chr(i) for i in c]
-    s = ''.join(d)
-    # print(d)
-    with open("Jarvis.txt", 'w+') as d:
+    # print(content, end='*'*50+'\n')
+    c = [i^3 for i in content]
+    # print(c, end="*"*50+'\n')
+    # c = bytes([i^3 for i in c])
+    # print(c)
+    s = bytes(c)
+    # s = ''.join(d)
+    # print(d, end="*"*50+'\n')
+    # k = bytes([i^3 for i in d])
+    # print(k, end="*"*50+'\n')
+    with open("Jarvis.txt", 'wb') as d:
         d.write(s)
 
 
